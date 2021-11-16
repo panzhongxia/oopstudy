01cc648646475b907257797441beb83c9c8ea5dc
panzhongxia <9954823+panzhongxia@user.noreply.gitee.com>
2021-11-16 23:29:28 +0800

    leetcode

--- src/letcode/num283.java
+++ src/letcode/num283.java
@@ -5,8 +5,16 @@ package letcode;
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  * https://leetcode-cn.com/problems/move-zeroes/
  */
-public class Movezeroes {
-    public int[] moveZeroes(int[] nums){
+public class num283 {
+    public static void main(String[] args) {
+        int[] input = {1,2,4,0,0,5,6};
+        int[] output = moveZeroes(input);
+        int length = output.length;
+        for (int i = 0; i < length; i++) {
+            System.out.println(output[i]);
+        }
+    }
+    public static int[] moveZeroes(int[] nums){
         int j = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != 0){
