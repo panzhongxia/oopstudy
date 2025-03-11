31d099339f7b67bcaa8c055e4de3cb1be786f56c
panzhongxia <panzhongxia@zhejianglab.com>
2025-03-11 10:46:24 +0800

    leetcode

--- src/letcode/array/num189.java
+++ src/letcode/array/num189.java
@@ -22,4 +22,22 @@ public class num189 {
         }
         System.arraycopy(newArr, 0, nums, 0, n);
     }
+
+    public void rotate1(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
+
 }
